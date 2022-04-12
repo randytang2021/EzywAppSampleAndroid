d975254ddeac3c97965e52e94b0742bde495ec11
Randy <rtang@wyze.com>
2022-04-12 16:22:19 -0700

    extract function

--- app/src/main/java/com/ezyw/sample/MainActivity.kt
+++ app/src/main/java/com/ezyw/sample/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.ezyw.sample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AppCompatActivity
+import com.ezyw.sample.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private val pluginClass = "com.sweet.plugin.sample.SampleMainActivity"
+    private var activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = activityMainBinding!!
+    private val activityResultLauncher = registerForActivityResult(
+        StartActivityForResult()
+    ) { result ->
+        val resultCode = result.resultCode
+        if (resultCode == pluginClass.hashCode()) {
+            val intent = result.data
+            if (intent != null) {
+                val resultFromPlugin = intent.getStringExtra("PLUGIN_DONE")
+                Toast.makeText(
+                    baseContext,
+                    "result from: $resultFromPlugin",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+                binding.appContentTv.append("result:${resultFromPlugin}\n")
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        launchActivityByQualifiedName(pluginClass, baseContext, activityResultLauncher)
+
+    }
+
+}
+
+fun launchActivityByQualifiedName(
+    pluginQualifiedName: String,
+    context: Context?,
+    activityResultLauncher1: ActivityResultLauncher<Intent>
+) {
+    try {
+        Class.forName(pluginQualifiedName)
+        val intent = Intent()
+        intent.component = ComponentName(
+            BuildConfig.APPLICATION_ID,
+            pluginQualifiedName
+        )
+        activityResultLauncher1.launch(intent)
+    } catch (e: ClassNotFoundException) {
+        Toast.makeText(
+            context,
+            "please register: $pluginQualifiedName or implement its aar dependency",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
